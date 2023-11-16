1e22ffeafd5a3c16f2922a76d4767cbfb4228df7
asha-cr99 <139260353+asha-cr99@users.noreply.github.com>
2023-11-16 20:40:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import ShoppingCart from './Components/ShoppingCart';
+import Timer from './Components/useTimer';
+
+function App() {
+  return (
+   
+    <div className="App"> 
+      
+      <Timer />
+      <ShoppingCart />    
+    </div>
+    
+  );
+}
+
+export default App;
+
